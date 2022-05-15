aae22e86bc91cddf60dc70d3c88de1170b2da294
Fatpandac <i@Fatpandac.com>
2022-05-15 14:46:48 +0800

    init: 🎉

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <hello-world></hello-world>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="h-screen w-screen flex justify-center items-center">
+    <span class="op30 hover:op80 ">Hello World</span>
+  </div>
+</template>
+
